522667f6b19e373cb2c98c73aa5f07141b5827b6
AaronCur <C00202505@itcarlow.ie>
2019-03-03 13:00:01 +0000

    Updated agents

--- connect4(PYGAME)/agents/best_move_agent.py
+++ connect4(PYGAME)/agents/best_move_agent.py
@@ -3,66 +3,6 @@ import random
 class BestMoveAgent():
     def __init__(self):
          self.tag = "BestMove"
-         
-    def evaluate_window(window, piece):
-        score = 0
-        opp_piece = PLAYER_PIECE
-        if piece == PLAYER_PIECE:
-            opp_piece = AI_PIECE
-
-        if window.count(piece) == 4:
-            score += 100
-        elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-            score += 5
-        elif window.count(piece) == 2 and window.count(EMPTY) == 2:
-            score += 2
-
-        if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
-            score -= 4
-
-        return score
-
-    def score_position(board, piece):
-        score = 0
-
-        ## Score center column
-        center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
-        center_count = center_array.count(piece)
-        score += center_count * 3
-
-        ## Score Horizontal
-        for r in range(ROW_COUNT):
-            row_array = [int(i) for i in list(board[r,:])]
-            for c in range(COLUMN_COUNT-3):
-                window = row_array[c:c+WINDOW_LENGTH]
-                score += evaluate_window(window, piece)
-
-        ## Score Vertical
-        for c in range(COLUMN_COUNT):
-            col_array = [int(i) for i in list(board[:,c])]
-            for r in range(ROW_COUNT-3):
-                window = col_array[r:r+WINDOW_LENGTH]
-                score += evaluate_window(window, piece)
-
-        ## Score posiive sloped diagonal
-        for r in range(ROW_COUNT-3):
-            for c in range(COLUMN_COUNT-3):
-                window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
-                score += evaluate_window(window, piece)
-
-        for r in range(ROW_COUNT-3):
-            for c in range(COLUMN_COUNT-3):
-                window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
-                score += evaluate_window(window, piece)
-
-        return score
-
-    def get_valid_locations(board):
-        valid_locations = []
-        for col in range(COLUMN_COUNT):
-            if is_valid_location(board, col):
-                valid_locations.append(col)
-        return valid_locations
 
     def pick_best_move(board, piece):
 
